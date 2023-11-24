4a30af7dad53a0480ba643b8d0786488d33f0cab
sagar <sagarsurendhra@gmail.com>
2023-11-24 07:37:52 +0530

    new changes

--- src/Component/BrowserContent/Resume/WorkExperience.js
+++ src/Component/BrowserContent/Resume/WorkExperience.js
@@ -8,11 +8,11 @@ export default function WorkExperience() {
     {ExpData.map((e,i) => (
       <div className='mb-4 pb-4 border-bottom'>             
         <div className='col-12 d-sm-flex mb-3 justify-content-between'>
-        <img className='companyLogo' src={`./IMG/${e.logo}`}/><br></br>
+        <h6>{e.logo}</h6>
         <small className='text-secondary'>{e.date}</small>
         </div>
         <p><b className='text-seondary'>{e.designation}</b></p>
-        <p>{e.desc}</p>
+        <p>{e.desc}</p><br/>
         <h6 className='text-secondary'>
           <a href={e.website} target='_blank'>website</a> | <a href={e.linkedin}>linkedin</a>
         </h6>          
